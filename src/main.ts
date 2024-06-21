c7d4143786d0a936269753258b08a590fafea4e5
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-21 14:09:52 -0600

    Reapply "Project Cybermerch initialized"
    This reverts commit a9ece8b5a9beaf1656a030ecb9690547df6cbd7b.

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`App running on port ${process.env.PORT}`);
+}
+bootstrap();
