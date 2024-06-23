25d0ec0f9eeb8ec6880c5fe66438a5e4605b86d3
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-22 21:25:27 -0600

    Entities created with documentation

---
+++ src/common/entities/user.entity.ts
@@ -0,0 +1,69 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Roles } from '../interfaces';
+
+export class User {
+  @ApiProperty({
+    example: '1e10e09d-eb51-409b-9770-21f6a8a84799',
+    description: 'User ID (UUID)',
+    uniqueItems: true,
+  })
+  id: string;
+
+  @ApiProperty({
+    description: 'User name',
+    example: 'Juan Perez',
+    nullable: false,
+    minLength: 1,
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 'juan_pe',
+    description: 'User tag identifier',
+    nullable: false,
+  })
+  tagName: string;
+
+  @ApiProperty({
+    description: 'User email',
+    example: 'juan1@gmail.com',
+    nullable: false,
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'User Password - It is encrypted when saved',
+    nullable: false,
+    minLength: 12,
+    maxLength: 50,
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'Validated email - Default(false)',
+    example: false,
+    default: false,
+  })
+  emailValidated?: boolean;
+
+  @ApiProperty({
+    description: 'This value is assigned by default as "user" by PrismaORM',
+    example: ['ADMIN'],
+    default: ['USER'],
+  })
+  roles: Roles[];
+
+  @ApiProperty({
+    description: 'User-created products',
+    example: ['Runner Max Shoes'],
+    default: [],
+  })
+  products?: string[];
+
+  @ApiProperty({
+    description: 'User-created products',
+    example: ['http://userImage.png'],
+    default: [],
+  })
+  useImg?: string;
+}
