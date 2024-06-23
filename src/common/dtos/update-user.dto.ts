4b040a86fba06e90c8fbc8e559eb4d34843be2df
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-22 21:24:39 -0600

    Dtos documentation added

--- src/common/dtos/update-user.dto.ts
+++ src/common/dtos/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { UserDto } from './user.dto';
 
 export class UpdateUserDto extends PartialType(UserDto) {}
