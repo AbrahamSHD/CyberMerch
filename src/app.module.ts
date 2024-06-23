f54b76290d07d8eb6ea0297054c5fbdb11d69339
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-22 20:34:34 -0600

    Other changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductsModule, AuthModule, CommonModule],
+  imports: [ConfigModule.forRoot(), ProductsModule, AuthModule, CommonModule, UsersModule],
 })
 export class AppModule {}
