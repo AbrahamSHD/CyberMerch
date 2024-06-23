2f4a0047bde45c693c79c15f7ecd3300a397f20c
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-22 20:40:42 -0600

    Dtos created with a swagger documentation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsNumber,
@@ -9,22 +10,47 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product name',
+    example: 'Runner Max Shoes',
+    nullable: false,
+    minLength: 2,
+    maxLength: 100,
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(100)
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'General information about the product',
+    example:
+      'Ergonomic Design: These shoes are designed to adapt to the natural shape of the foot, offering a comfortable and secure fit.',
+    nullable: true,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   @MaxLength(500)
   @IsOptional()
   description: string;
 
+  @ApiProperty({
+    description: 'Product cost',
+    example: '89.90',
+    nullable: false,
+  })
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty({
+    description: 'ID of the user who created the product',
+    example: '87151d0b-0624-44c4-bf68-0ce921fc8ff0',
+    nullable: false,
+    minLength: 1,
+  })
   @IsUUID()
   @IsNotEmpty()
   userId: string;
