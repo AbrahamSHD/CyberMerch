d7de10f345b9a1f890509972cd0a7184857a60bd
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-06-24 17:49:24 -0600

    Auth service created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from '../common/dtos/user.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
+  login(loginUserDto: UserDto) {
     return `This action generates a login`;
   }
-  logout() {
+  logout(logoutUserDto: UserDto) {
     return `This action generates a logout`;
   }
 }
