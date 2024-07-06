cbb05e5d69bf758afefa077704b7d4e494a4bc84
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-07-05 22:03:18 -0600

    Files Module Created

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,9 +1,73 @@
-import { Controller } from '@nestjs/common';
-import { FilesService } from './files.service';
+import {
+  BadRequestException,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
+import { fileFilter, fileNamer } from './helpers';
 
 @ApiTags('Files - Get and Upload')
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:image')
+  findOne(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticImage(imageName);
+
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1000 }
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+    return { secureUrl };
+  }
+
+  @Post('product2')
+  uploadFileTwo(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+          new FileTypeValidator({ fileType: 'image/png' }),
+          new FileTypeValidator({ fileType: 'image/jpg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return file;
+  }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  getStaticImage(imageName: string) {
+    return imageName;
+  }
+}
